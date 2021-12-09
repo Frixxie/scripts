5f4d31672399c1aab12dcfca55c83ebbdfa2981d
Fredrik <freddahf@outlook.com>
2021-12-09 09:44:56 +0100

    added newline in hevn script

--- hevn.sh
+++ hevn.sh
@@ -3,4 +3,5 @@
 tail -n 2 ${HOME}/projects/weather/hevn.log \
     | tr '\n' ' ' | tr ',' ' ' \
     | awk '{printf "%s,%s°C,%s%%,%s,%s°C,%s%%", $1, $2, $3, $4, $5, $6}' \
-    | sed 's/\([1-9][0-9]\)\([0-9]*\)/\1.\2/g'
+    | sed 's/\([1-9][0-9]\)\([0-9]*\)/\1.\2/g' \
+    | awk '{print $0}'
