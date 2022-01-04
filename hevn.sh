aa08d5ea1f5af6cb77829a16cda757f1f40d4607
fredrik <freddahf@outlook.com>
2022-01-04 15:51:21 +0100

    updated hevn.sh

--- hevn.sh
+++ hevn.sh
@@ -3,5 +3,3 @@
 tail -n 2 ${HOME}/projects/weather/hevn.log \
     | tr '\n' ' ' | tr ',' ' ' \
     | awk '{printf "%s,%s°C,%s%%,%s,%s°C,%s%%", $1, $2, $3, $4, $5, $6}' \
-    | sed 's/\([1-9][0-9]\)\([0-9]*\)/\1.\2/g' \
-    | awk '{print $0}'
